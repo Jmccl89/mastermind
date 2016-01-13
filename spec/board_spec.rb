9fab4caf6fb0cda61be399de903514aa4d8e1834
James McCloskey <james@mccloskey.io>
2016-01-13 11:30:03 -0600

    initial board spec

---
+++ spec/board_spec.rb
@@ -0,0 +1,9 @@
+# spec/board_spec.rb
+require 'spec_helper'
+
+module Mastermind
+  describe Board do
+    describe '#initialize' do
+      it 'initializes the board with a number of guesses and a solution' do
+      end
+
